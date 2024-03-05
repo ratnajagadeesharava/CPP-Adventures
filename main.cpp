e9b6d7c7fa98679e575aa6635a857c0d7d3fffdd
Ratna Jagadeesh <35951109+Ratnajagadeesharava@users.noreply.github.com>
2024-03-05 21:05:51 +0530

    added vector

--- main.cpp
+++ main.cpp
@@ -4,9 +4,5 @@
 #include "threads.h"
 using namespace CPP_Adventures;
 void main() {
-	std::cout << "Hello World" << std::endl;
-	//Threads::call_threads(10);
-	//Threads::MakeForecasts();
-	Threads::RunMutex();
-	lambdas lam;
+	
 }
